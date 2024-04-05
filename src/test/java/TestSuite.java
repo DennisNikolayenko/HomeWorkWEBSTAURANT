71292e76b040ee939725d3fd4e77129a4c35edb3
DennisNikolayenko <67177693+DennisNikolayenko@users.noreply.github.com>
2024-04-05 09:06:17 -0400

    Delete src directory

--- src/test/java/TestSuite.java
+++
@@ -1,9 +0,0 @@
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-import Steps.SearchTest;
-
-@RunWith(Suite.class)
-@Suite.SuiteClasses({SearchTest.class})
-
-public class TestSuite {
-}
